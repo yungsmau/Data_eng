8eb5447fee7d622fd3b6ecaedc4e5d83cf1fde46
yungsmau <smoli4.alek@yandex.ru>
2024-11-06 12:02:28 +0500

    5, 6

---
+++ prac01/06.py
@@ -0,0 +1,10 @@
+import requests
+import pandas as pd
+
+response = requests.get("https://api.coindesk.com/v1/bpi/currentprice.json")
+data = response.json()
+
+df = pd.DataFrame(data["bpi"]).T
+df.to_html("answers/new_html_from_json.html", border=1, justify="center")
+
+print("Таблица сохранена в new_html_from_json.html")
